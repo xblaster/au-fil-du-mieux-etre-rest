ea512169d182af16dc55ed0e76bcfb6e36d77e2b
jerome wax <xblaster@gmail.com>
2022-01-23 22:45:37 +0100

    fix mapping

--- routes/posts.js
+++ routes/posts.js
@@ -14,13 +14,29 @@ const requestify = require('requestify');
  *       200:
  *         description: Returns a mysterious string.
  */
+
+function mapResult(body) {
+  const resultBody = body.map((json) => ({
+    id: json.id,
+    title: json.title.rendered,
+  }));
+
+  return resultBody;
+}
+
 router.get('/', (req, res) => {
   requestify.get(`${baseUrl}/wp/v2/posts`).then((response) => {
     // Get the response body (JSON parsed - JSON response or jQuery object in case of XML response)
-    response.getBody();
+    const body = response.getBody();
+
+    const resultBody = mapResult(body);
+    console.log(resultBody);
 
     // Get the response raw body
-    res.send(response.body);
+    /* res.send(response.body.map(
+      (elt) => elt.id,
+    ));   */
+    res.send(resultBody);
   });
 });
 
